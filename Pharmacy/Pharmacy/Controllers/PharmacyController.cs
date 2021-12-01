6634ed884b33dae8150bf291fc45fa1fe07a663d
iftikharbg <iftikharbg@code.edu.az>
2021-12-02 01:03:18 +0400

    Homework

---
+++ Pharmacy/Pharmacy/Controllers/PharmacyController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pharmacy.Models;
+using Pharmacy.ViewsModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Controllers
+{
+    public class PharmacyController : Controller
+    {
+        public IActionResult Drug()
+        {
+            List<Drug> drugs = new List<Drug>()
+            {
+                new Drug(){Name="Gliaton Forte",Price=28,Company="Fortex"},
+                new Drug(){Name="Arko Krem",Price=2,Company="Eviap"},
+                new Drug(){Name="Santa Farma",Price=3,Company="Alset"}
+                
+            };
+            List<DrugType> types = new List<DrugType>()
+            {
+                new DrugType(){Type="Vitamin"},
+                new DrugType(){Type="Krem"}
+            };
+            DrugViewModel model = new DrugViewModel()
+            {
+                Drugs = drugs,
+                DrugTypes = types
+            };
+            return View(model); 
+        }
+
+        public IActionResult DrugType()
+        {
+            List<DrugType> types = new List<DrugType>()
+            {
+                new DrugType(){Type="Vitamin"},
+                new DrugType(){Type="Krem"}
+            };
+            return View(types);
+        }
+    }
+}
---
+++ Pharmacy/Pharmacy/ViewsModels/DrugViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Pharmacy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacy.ViewsModels
+{
+    public class DrugViewModel
+    {
+        public List<Drug> Drugs { get; set; }
+
+        public List<DrugType> DrugTypes { get; set; }
+    }
+}
